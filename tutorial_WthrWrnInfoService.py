f1c5d3d602c9213e3a4b2d75ddf1ae676170892c
taeyoon-1991 <33391147+taeyoon-1991@users.noreply.github.com>
2021-04-26 21:26:17 +0900

    Update tutorial_WthrWrnInfoService.py

--- tutorial_WthrWrnInfoService.py
+++ tutorial_WthrWrnInfoService.py
@@ -9,7 +9,7 @@
 # Email: eom.taeyoon.kor@gmail.com
 ###########################################################
 
-from openAPI_KMA import WthrWrnInfoService
+from openKMA import WthrWrnInfoService
 from datetime import datetime, timedelta
 
 ServiceKey = " 이곳에 Data.go.kr에서 발급받은 ServiceKey를 입력하세요. "
