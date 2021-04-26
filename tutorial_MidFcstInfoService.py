f15779585cc652fc3cfadd3de1377b46d5a014e0
taeyoon-1991 <33391147+taeyoon-1991@users.noreply.github.com>
2021-04-26 21:25:38 +0900

    Update tutorial_MidFcstInfoService.py

--- tutorial_MidFcstInfoService.py
+++ tutorial_MidFcstInfoService.py
@@ -9,7 +9,7 @@
 # Email: eom.taeyoon.kor@gmail.com
 ###########################################################
 
-from openAPI_KMA import MidFcstInfoService
+from openKMA import MidFcstInfoService
 from datetime import datetime, time
 
 ServiceKey = " 이곳에 Data.go.kr에서 발급받은 ServiceKey를 입력하세요. "
