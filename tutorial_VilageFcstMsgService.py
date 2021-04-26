d51d48fce9343309b546ea9844b4d669efb7a6b4
taeyoon-1991 <33391147+taeyoon-1991@users.noreply.github.com>
2021-04-26 21:26:01 +0900

    Update tutorial_VilageFcstMsgService.py

--- tutorial_VilageFcstMsgService.py
+++ tutorial_VilageFcstMsgService.py
@@ -9,7 +9,7 @@
 # Email: eom.taeyoon.kor@gmail.com
 ###########################################################
 
-from openAPI_KMA import VilageFcstMsgService
+from openKMA import VilageFcstMsgService
 
 ServiceKey = " 이곳에 Data.go.kr에서 발급받은 ServiceKey를 입력하세요. "
 
